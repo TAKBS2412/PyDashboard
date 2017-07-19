ac46506338123455bf79d8eb1418f9b14a9f8dd9
Timothy Eng <timothyeng36@gmail.com>
2017-07-19 16:05:45 -0700

    Added another frame inside the first one.

--- PyDashboard.py
+++ PyDashboard.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+class Chooser(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.createWidgets()
+    def createWidgets(self):
+        self.header = tk.Label(self, text="Step 1:").grid(row=0, column=0)
+        self.header2 = tk.Label(self, text="sdf").grid(row=1, column=0)
+        
 class PyDashboard(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -7,6 +15,8 @@ class PyDashboard(tk.Frame):
         self.createWidgets()
     def createWidgets(self):
         self.header = tk.Label(self, text="PyDashboard").grid(row=0, column=0)
+        self.frame0 = Chooser().grid(row=1, column=0)
+        
 dashboard = PyDashboard()
 dashboard.master.title("PyDashboard")
 dashboard.mainloop()
