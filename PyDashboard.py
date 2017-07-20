2077163252728dc2fe1fc3afc2faa129a7b6a904
Timothy Eng <timothyeng36@gmail.com>
2017-07-19 19:50:43 -0700

    Added imgpath parameter.

--- PyDashboard.py
+++ PyDashboard.py
@@ -2,11 +2,12 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
 class Chooser(ttk.LabelFrame):
-    def __init__(self, master=None, column=0, headertext="", comboboxvalues=(), **kw):
+    def __init__(self, master=None, column=0, headertext="", comboboxvalues=(), imgpath="", **kw):
         ttk.LabelFrame.__init__(self, master, **kw)
         self.column = column
         self.headertext = headertext
         self.comboboxvalues = comboboxvalues
+        self.imgpath = imgpath
         self.createWidgets()
     def createWidgets(self):
         self.header = ttk.Label(self, text=self.headertext)
@@ -17,7 +18,7 @@ class Chooser(ttk.LabelFrame):
         self.dropdown["values"] = self.comboboxvalues
         self.dropdown.grid(row=2, column=self.column)
 
-        self.image = ImageTk.PhotoImage(Image.open("imgs/Steampunk RT_icon.png"))
+        self.image = ImageTk.PhotoImage(Image.open(self.imgpath))
         self.imagelabel = ttk.Label(self, image=self.image)
         self.imagelabel.image = self.image
         self.imagelabel.grid(row=3, column=self.column)
@@ -34,7 +35,7 @@ class PyDashboard(ttk.Frame):
         self.pane = ttk.PanedWindow(self, orient=HORIZONTAL)
         self.pane.grid(row=1, column=0, columnspan=2)
         
-        self.robotStatus = Chooser(self.pane, 0, "Step 1", ("A", "B", "C"), text="Robot Status", width=1920, height=1080)
+        self.robotStatus = Chooser(self.pane, 0, "Step 1", ("A", "B", "C"), "imgs/Steampunk RT_icon.png", text="Robot Status", width=1920, height=1080)
         self.robotStatus.grid(row=1, column=0, rowspan=2, columnspan=2)
         
         self.pane.add(self.robotStatus)
