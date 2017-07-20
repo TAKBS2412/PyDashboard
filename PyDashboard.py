1c350c151bfc9e0f455af3731adc31cd788d3a56
Timothy Eng <timothyeng36@gmail.com>
2017-07-19 18:22:11 -0700

    Changed Chooser to a ttk.LabelFrame.

--- PyDashboard.py
+++ PyDashboard.py
@@ -1,13 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 
-class Chooser(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
+class Chooser(ttk.LabelFrame):
+    def __init__(self, master=None, **kw):
+        ttk.LabelFrame.__init__(self, master, **kw)
         self.createWidgets()
     def createWidgets(self):
-        self.header = ttk.Label(self.master, text="Step 1:").grid(row=2, column=0)
-        self.header2 = ttk.Label(self.master, text="sdf").grid(row=3, column=0)
+        self.header = ttk.Label(self, text="Step 1:")
+        self.header.grid(row=1)
+        self.header2 = ttk.Label(self, text="sdf")
+        self.header2.grid(row=2)
         
 class PyDashboard(ttk.Frame):
     def __init__(self, master=None):
@@ -15,8 +17,16 @@ class PyDashboard(ttk.Frame):
         self.grid()
         self.createWidgets()
     def createWidgets(self):
-        self.header = ttk.Label(self, text="PyDashboard").grid(row=0, column=0)
-        self.frame0 = Chooser(self).grid(row=1, column=0)
+        self.header = ttk.Label(self, text="PyDashboard")
+        self.header.grid(row=0, column=0)
+        
+        self.pane = ttk.PanedWindow(self, orient=HORIZONTAL)
+        self.pane.grid(row=1, column=0, columnspan=2)
+        
+        self.robotStatus = Chooser(self.pane, text="Robot Status", width=500, height=500)
+        self.robotStatus.grid(row=1, column=0, rowspan=2, columnspan=2)
+        
+        self.pane.add(self.robotStatus)
         
 dashboard = PyDashboard(Tk())
 dashboard.master.title("PyDashboard")
