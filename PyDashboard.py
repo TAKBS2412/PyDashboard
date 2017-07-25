c00793bca1372f75cb7141176805b5c7ac5862e5
Timothy Eng <timothyeng36@gmail.com>
2017-07-24 18:46:11 -0700

    Reorganized layout.
    2 PanedWindows with 2 Choosers each.

--- PyDashboard.py
+++ PyDashboard.py
@@ -22,15 +22,17 @@ class Chooser(ttk.LabelFrame):
     Creates a Chooser instance (see above description).
     Parameters:
         master - The master Frame.
+        row - The row in the grid that this Chooser will occupy.
         column - The column in the grid that this Chooser will occupy.
         headertext - The text that the header Label will display.
         comboboxvalues - A tuple of values that the Combobox will display.
         imgpath - The path to the image (as a string) that will be displayed.
     '''
-    def __init__(self, master=None, column=0, headertext="", comboboxvalues=(), imgpath="", **kw):
+    def __init__(self, master=None, row=0, column=0, headertext="", comboboxvalues=(), imgpath="", **kw):
         ttk.LabelFrame.__init__(self, master, **kw) # Call the superclass's constructor.
 
         # Set the values that were passed in as parameters.
+        self.row = row
         self.column = column
         self.headertext = headertext
         self.comboboxvalues = comboboxvalues
@@ -40,19 +42,19 @@ class Chooser(ttk.LabelFrame):
     def createWidgets(self):
         # Create a Label with the text specified (self.headertext).
         self.header = ttk.Label(self, text=self.headertext) # Create the Label itself.
-        self.header.grid(row=1, column=self.column) # Add to Frame.
+        self.header.grid(row=self.row, column=self.column) # Add to Frame.
 
         # Create a drop down menu (AKA Combobox) from the values specified (self.comboboxvalues).
         self.dropdownvar = StringVar() # Create a string variable (StringVar) that will hold the currently selected value.
         self.dropdown = ttk.Combobox(self, textvariable=self.dropdownvar) # Create the Combobox itself.
         self.dropdown["values"] = self.comboboxvalues # Set the values that were specified in the constructor.
-        self.dropdown.grid(row=2, column=self.column) # Add to Frame.
+        self.dropdown.grid(row=self.row+1, column=self.column) # Add to Frame.
 
         # Create an image from the path specified (self.imgpath) and display it to the user using a Label.
         self.image = ImageTk.PhotoImage(Image.open(self.imgpath)) # Load the image.
         self.imagelabel = ttk.Label(self, image=self.image) # Create the Label that will be used to display the image.
         self.imagelabel.image = self.image # Make sure to keep a reference to the image - see http://effbot.org/tkinterbook/photoimage.htm.
-        self.imagelabel.grid(row=3, column=self.column) # Add to Frame.
+        self.imagelabel.grid(row=self.row+2, column=self.column) # Add to Frame.
 
 '''
 This class represents the GUI for PyDashboard.
@@ -113,17 +115,41 @@ class PyDashboard(ttk.Frame):
         self.sendbtn = ttk.Button(self, text="Send", command=send) # Create the Send button.
         self.sendbtn.grid(row=4, column=2) # Add the Send button to the PyDashboard.
 
-        # Create the PanedWindow, which will contain the Robot Status Label and the Choosers.
-        self.pane = ttk.PanedWindow(self, orient=HORIZONTAL) # Create the PanedWindow.
-        self.pane.grid(row=1, column=0) # Add the PanedWindow to the PyDashboard.
-
         # Create the Robot Status LabelFrame.
-        self.robotstatus = ttk.LabelFrame(self.pane, text="Robot Status") # Create the LabelFrame itself.
+        self.robotstatus = ttk.LabelFrame(self, text="Robot Status") # Create the LabelFrame itself.
         self.robotstatus.label = ttk.Label(self.robotstatus, text="Robot Disconnected") # Create the Label that will be displayed within the LabelFrame.
         self.robotstatus.label.grid(row=1, column=0) # Add the Label to the LabelFrame.
-        self.robotstatus.grid(row=1, column=0) # Add the LabelFrame to the PyDashboard.
-        self.pane.add(self.robotstatus) # Add the LabelFrame to the PanedWindow.
+        self.robotstatus.grid(row=1, column=0, rowspan=2) # Add the LabelFrame to the PyDashboard.
 
+        # Create a PanedWindow, which will contain the Choosers for steps 1 and 2.
+        self.pane1and2 = ttk.PanedWindow(self, orient=HORIZONTAL) # Create the PanedWindow.
+        self.pane1and2.grid(row=1, column=1) # Add the PanedWindow to the PyDashboard.
+
+        # Create and add Step 1 Chooser.
+        self.step1 = Chooser(self.pane1and2, 1, 1, self.headertexttup[0], self.comboboxvaluestup[0], self.imgpathtup[0], text=self.titletup[0])
+        self.step1.grid(row=1, column=1) # Add Step 1 Chooser to the PyDashboard.
+        self.pane1and2.add(self.step1) # Add the Chooser to the PanedWindow.
+
+        # Create and add Step 2 Chooser.
+        self.step2 = Chooser(self.pane1and2, 1, 2, self.headertexttup[1], self.comboboxvaluestup[1], self.imgpathtup[1], text=self.titletup[1])
+        self.step2.grid(row=1, column=2) # Add Step 2 Chooser to the PyDashboard.
+        self.pane1and2.add(self.step2) # Add the Chooser to the PanedWindow.
+
+        # Create a PanedWindow, which will contain the Choosers for steps 3 and 4.
+        self.pane3and4 = ttk.PanedWindow(self, orient=HORIZONTAL) # Create the PanedWindow.
+        self.pane3and4.grid(row=2, column=1) # Add the PanedWindow to the PyDashboard.
+
+        # Create and add Step 3 Chooser.
+        self.step3 = Chooser(self.pane3and4, 2, 1, self.headertexttup[2], self.comboboxvaluestup[2], self.imgpathtup[2], text=self.titletup[2])
+        self.step3.grid(row=2, column=1) # Add Step 3 Chooser to the PyDashboard.
+        self.pane3and4.add(self.step3) # Add the Chooser to the PanedWindow.
+
+        # Create and add Step 4 Chooser.
+        self.step4 = Chooser(self.pane3and4, 2, 2, self.headertexttup[3], self.comboboxvaluestup[3], self.imgpathtup[3], text=self.titletup[3])
+        self.step4.grid(row=2, column=2) # Add Step 4 Chooser to the PyDashboard.
+        self.pane3and4.add(self.step4) # Add the Chooser to the PanedWindow.
+
+        '''
         # Create an array of all of the choosers and add them.
         self.choosers = []
 
@@ -132,6 +158,7 @@ class PyDashboard(ttk.Frame):
             self.choosers.append(Chooser(self.pane, zipobj[0]+1, zipobj[1], zipobj[2], zipobj[3], text=zipobj[4])) # Create a Chooser.
             self.choosers[zipobj[0]].grid(row=1, column=zipobj[0]+1) # Add the Chooser to the PyDashboard.
             self.pane.add(self.choosers[zipobj[0]]) # Add the Chooser to the PanedWindow.
+        '''
 
 title = "PyDashboard"
 dashboard = PyDashboard(Tk(), title, ("Choose an autonomous mode:", "Falca will drive forward to the baseline using...", "Falca will turn towards the peg using...", "Falca will drive towards the peg using..."), (("Drive forward (if selected, ignore Steps 3 and 4)", "Left Peg", "Center Peg (if selected, ignore Step 3", "Right Peg"), ("Motion Profiling", "Time-Based", "Encoders"), ("Vision Processing", "Gyroscope"), ("Vision Processing", "Encoders")), ("imgs/Step1.png", "imgs/Step2.png", "imgs/Step3.png", "imgs/Step4.png"), ("Step 1", "Step 2", "Step 3", "Step 4"))
