3a3e1958e315bbb62ddc63cc7c75f861837fb874
Timothy Eng <timothyeng36@gmail.com>
2017-07-25 17:37:49 -0700

    Created (empty) DataItem class.
    The DataItem class will contain the data for a Chooser that will be sent to the user via pynetworktables.

--- Data.py
+++ Data.py
@@ -1,4 +1,11 @@
 '''
+This class contains the data for a single Chooser that will be sent to the user.
+'''
+
+class DataItem:
+    pass
+
+'''
 This class contains the data that will be sent to the user.
 '''
 
