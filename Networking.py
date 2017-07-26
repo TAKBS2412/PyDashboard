7ac27dd1d6f266b01b0af5bd1fd435212c7b4b63
Timothy Eng <timothyeng36@gmail.com>
2017-07-25 19:09:29 -0700

    Added Networking constructor.

--- Networking.py
+++ Networking.py
@@ -1,5 +1,18 @@
+from networktables import NetworkTables
+
 '''
 Sends data over to the user using NetworkTables.
 '''
 class Networking:
-    pass
+
+    '''
+    Creates a Networking instance (see above description).
+    Parameters:
+        ip - The ip address to connect to.
+        tablename - The name of the NetworkTable to connect to.
+    '''
+    def __init__(self, ip="roborio-2412-frc.local", tablename="PyDashboard"):
+        # Setup NetworkTables
+        NetworkTables.initialize(server=ip)
+        self.table = NetworkTables.getTable(tablename)
+        
